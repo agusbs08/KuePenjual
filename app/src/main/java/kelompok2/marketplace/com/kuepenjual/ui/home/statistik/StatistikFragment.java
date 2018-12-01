48dddb10b21e33087f203750f46390a78165d59d
ninjaonion <kuwagataasrokhuso@gmail.com>
2018-12-01 13:43:35 +0700

    x

--- app/src/main/java/kelompok2/marketplace/com/kuepenjual/ui/home/statistik/StatistikFragment.java
+++ app/src/main/java/kelompok2/marketplace/com/kuepenjual/ui/home/statistik/StatistikFragment.java
@@ -1,7 +1,44 @@
 package kelompok2.marketplace.com.kuepenjual.ui.home.statistik;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import kelompok2.marketplace.com.kuepenjual.R;
 
 public class StatistikFragment extends Fragment {
 
+    private GraphView graph;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_statistik, container, false);
+        initView(rootView);
+        return rootView;
+    }
+
+    private void initView(View view){
+        graph = view.findViewById(R.id.graph);
+    }
 }
