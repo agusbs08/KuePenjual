ff1efab9f3c107be00203e72d5a98463a84307ea
Pahlevy18 <ridhopahlevy@gmail.com>
2019-01-01 18:29:17 +0700

    1/1

--- app/src/main/java/kelompok2/marketplace/com/kuepenjual/ui/home/search/SearchRecyclerViewAdapter.java
+++ app/src/main/java/kelompok2/marketplace/com/kuepenjual/ui/home/search/SearchRecyclerViewAdapter.java
@@ -1,14 +1,18 @@
 package kelompok2.marketplace.com.kuepenjual.ui.home.search;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -19,25 +23,29 @@ import kelompok2.marketplace.com.kuepenjual.R;
 import kelompok2.marketplace.com.kuepenjual.model.Barang;
 import kelompok2.marketplace.com.kuepenjual.model.PenjualanBarangList;
 import kelompok2.marketplace.com.kuepenjual.ui.edit.UpdateActivity;
+import kelompok2.marketplace.com.kuepenjual.ui.home.HomeActivity;
 
 public class SearchRecyclerViewAdapter extends RecyclerView.Adapter<SearchRecyclerViewAdapter.SearchViewHolder> {
 
     private ArrayList<PenjualanBarangList> listBarang;
     private Context context;
-    public SearchRecyclerViewAdapter(ArrayList<PenjualanBarangList> listBarang,Context context){
+    private Intent intent;
+
+    public SearchRecyclerViewAdapter(ArrayList<PenjualanBarangList> listBarang,Context context, Intent intent){
         this.listBarang = listBarang;
         this.context = context;
+        this.intent = intent;
     }
 
     @NonNull
     @Override
     public SearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new SearchViewHolder(LayoutInflater.from(context).inflate(R.layout.list_search, parent, false), context);
+        return new SearchViewHolder(LayoutInflater.from(context).inflate(R.layout.list_search, parent, false), context,intent);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SearchViewHolder holder, int position) {
-        holder.bindItem(listBarang.get(position));
+        holder.bindItem(listBarang.get(position), listBarang.get(position).getIdPenjualan());
     }
 
     @Override
@@ -45,25 +53,40 @@ public class SearchRecyclerViewAdapter extends RecyclerView.Adapter<SearchRecycl
         return listBarang.size();
     }
 
-    class SearchViewHolder extends RecyclerView.ViewHolder{
+    class SearchViewHolder extends RecyclerView.ViewHolder implements SearchRecyclerViewView{
 
         private Context context;
         private TextView namaBarang;
         private ImageView imageBarang;
+        private TextView hargaBarang;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Intent intent;
+        private SearchRecyclerViewPresenter presenter;
 
-        public SearchViewHolder(View view, Context context){
+
+        public SearchViewHolder(View view, Context context, Intent intent){
             super(view);
             this.context = context;
+            this.intent = intent;
+            presenter = new SearchRecyclerViewPresenter(this);
             initView(view);
         }
 
         private void initView(View view){
             imageBarang = view.findViewById(R.id.img_fotobarang_list_search);
             namaBarang = view.findViewById(R.id.tv_namabarang_list_search);
+            hargaBarang = view.findViewById(R.id.tv_hargabarang_list_search);
+            btnUpdate = view.findViewById(R.id.btn_edit_list_search);
+            btnDelete = view.findViewById(R.id.btn_sampah_list_search);
         }
 
-        void bindItem(PenjualanBarangList penjualanBarangList){
+        void bindItem(PenjualanBarangList penjualanBarangList, Integer idPenjualan){
             final Barang barang = penjualanBarangList.getBarang();
+            initBtnUpdate(barang);
+            initBtnDelete(barang, idPenjualan);
+            Integer harga = barang.getHarga().intValue();
+            hargaBarang.setText("Rp " + harga.toString());
             Picasso.get().load(BuildConfig.BASE_STORAGE + barang.getGambar()).into(imageBarang);
             namaBarang.setText(barang.getNama());
             itemView.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +97,46 @@ public class SearchRecyclerViewAdapter extends RecyclerView.Adapter<SearchRecycl
                     context.startActivity(intent);
                 }
             });
+        }
+
+        private void initBtnUpdate(final Barang barang){
+            btnUpdate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, UpdateActivity.class);
+                    intent.putExtra("barang", barang);
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+        private void initBtnDelete(final Barang barang, final Integer idPenjualan){
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setMessage("Apa kamu yakin menghapus " + barang.getNama() + "?");
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("YA", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            presenter.deletePenjualan(idPenjualan, barang.getId());
+                        }
+                    });
+                    builder.setNegativeButton("TIDAK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+                    builder.show();
+                }
+            });
+        }
+
+        @Override
+        public void refreshLayout() {
+            Toast.makeText(context, "Barang Sudah Dihapus", Toast.LENGTH_SHORT).show();
+            intent = new Intent(context, HomeActivity.class);
+            context.startActivity(intent);
         }
     }
 }
---
+++ app/src/main/java/kelompok2/marketplace/com/kuepenjual/ui/home/search/SearchRecyclerViewPresenter.java
@@ -0,0 +1,63 @@
+package kelompok2.marketplace.com.kuepenjual.ui.home.search;
+
+import android.util.Log;
+
+import kelompok2.marketplace.com.kuepenjual.base.BasePresenterNetwork;
+import kelompok2.marketplace.com.kuepenjual.model.Barang;
+import kelompok2.marketplace.com.kuepenjual.model.response.ModelResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by ridho on 05/12/18.
+ */
+
+public class SearchRecyclerViewPresenter extends BasePresenterNetwork {
+    private SearchRecyclerViewView view;
+
+    public SearchRecyclerViewPresenter(SearchRecyclerViewView view){
+        super();
+        this.view = view;
+    }
+
+    public void deletePenjualan(Integer idPenjualan, final Integer id) {
+        Call<ModelResponse<Barang>> result = service.deletePenjualan(idPenjualan);
+        result.enqueue(new Callback<ModelResponse<Barang>>() {
+            @Override
+            public void onResponse(Call<ModelResponse<Barang>> call, Response<ModelResponse<Barang>> response) {
+                if(response.isSuccessful()){
+                    Log.e("deletePenjualan", response.body().getMessage());
+                    deleteBarang(id);
+                }
+                else{
+                    Log.e("deletePenjualanr", response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse<Barang>> call, Throwable t) {
+                Log.e("deletePenjualanf", t.getMessage());
+            }
+        });
+    }
+
+    private void deleteBarang(Integer id){
+        Call<ModelResponse<Barang>> result = service.deleteBarang(id);
+
+        result.enqueue(new Callback<ModelResponse<Barang>>() {
+            @Override
+            public void onResponse(Call<ModelResponse<Barang>> call, Response<ModelResponse<Barang>> response) {
+                if (response.isSuccessful()) {
+                    Log.e("deleteBarang", response.body().getMessage());
+                    view.refreshLayout();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ModelResponse<Barang>> call, Throwable t) {
+
+            }
+        });
+    }
+}
